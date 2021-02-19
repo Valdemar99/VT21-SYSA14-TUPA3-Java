52e668052e4dd7030f2b2daa97f720e61964d863
Valdemar99 <va7012an-s@student.lu.se>
2021-02-19 14:46:20 +0100

    Created project AGAIN

---
+++ src/GUIApplication/DataAccessLayer.java
@@ -0,0 +1,12 @@
+package GUIApplication;
+
+import java.rmi.RemoteException;
+
+import org.tempuri.WebService1SoapProxy;
+
+public class DataAccessLayer { 
+	public String readFile(String fileName) throws RemoteException {
+		WebService1SoapProxy proxy = new WebService1SoapProxy();
+		return proxy.readPath(fileName);
+	}
+}
